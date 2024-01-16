60b84b708992fd5e513a4a6877e018f2510924cb
Mantas Simanauskas <mantas.simanauskas@ikano.se>
2024-01-16 23:44:44 +0100

    Initial Commit

---
+++ MissingPeople/MissingPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingPeopleApp.swift
+//  MissingPeople
+//
+//  Created by Mantas Simanauskas on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct MissingPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
