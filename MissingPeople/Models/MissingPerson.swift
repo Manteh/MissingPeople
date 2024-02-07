3c5d601305636dd07798c6f0b790335f2e1b9a33
Mantas Simanauskas <mantehofficial@gmail.com>
2024-02-07 19:39:16 +0100

    Update `MissingPerson` object

--- MissingPeople/Models/MissingPerson.swift
+++ MissingPeople/Models/MissingPerson.swift
@@ -9,18 +9,18 @@ import Foundation
 
 struct MissingPerson: Identifiable, Codable {
     let id: UUID = UUID()
-    let age: String
-    let clothingAppearance: String
-    let gender: String
-    let hairAppearance: String
-    let hasBeard: String
-    let height: String
+    let age: String?
+    let clothingAppearance: String?
+    let gender: String?
+    let hairAppearance: String?
+    let hasBeard: String?
+    let height: String?
     let imageURL: String
-    let lastSeenAt: String
-    let missingSince: String
+    let lastSeenAt: String?
+    let missingSince: String?
     let name: String
-    let transportColor: String
-    let transportName: String
-    let transportRegistrationNumber: String
-    let weight: String
+    let transportColor: String?
+    let transportName: String?
+    let transportRegistrationNumber: String?
+    let weight: String?
 }
