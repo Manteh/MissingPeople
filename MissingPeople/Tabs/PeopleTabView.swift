3c496fd200e98cd08a8004d2c952d5dae0e201ab
Mantas Simanauskas <mantehofficial@gmail.com>
2024-02-27 11:35:27 +0100

    Added search bar to the list

--- MissingPeople/Tabs/PeopleTabView.swift
+++ MissingPeople/Tabs/PeopleTabView.swift
@@ -6,37 +6,67 @@
 //
 
 import SwiftUI
+import Combine
+
+class PeopleTabViewModel: ObservableObject {
+    @Published var text: String = ""
+}
 
 struct PeopleTabView: View {
-    @Binding var missingPeople: [MissingPerson]
+    @StateObject private var vm = PeopleTabViewModel()
+    @State var filteredMissingPeople: [MissingPerson] = []
+    @Binding var missingPeople: [MissingPerson] {
+        didSet { filteredMissingPeople = missingPeople }
+    }
 
     var body: some View {
         NavigationStack {
             ZStack {
                 Color("Background").edgesIgnoringSafeArea(.all)
-                List(missingPeople, id: \.id) { missingPerson in
-                    PeopleCardView(missingPerson: missingPerson)
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
+                VStack {
+                    HStack {
+                        if !missingPeople.isEmpty {
+                            Image(systemName: "magnifyingglass")
+                            TextField(text: $vm.text) {
+                                Text("Sök bland \(missingPeople.count) försvunna personer")
+                            }
+                        }
+                        Button(action: { $vm.text.wrappedValue = "" }, label: {
+                            Image(systemName: "xmark")
+                                .frame(width: 14, height: 14)
+                                .padding(10)
+                                .background(RoundedRectangle(cornerRadius: 10).opacity(0.05))
+                        })
+                        .opacity(vm.text.isEmpty ? 0 : 0.8)
+                        .scaleEffect(vm.text.isEmpty ? 0 : 1)
+                        .animation(.spring, value: vm.text.isEmpty)
+                    }
+                    .padding()
+                    List(filteredMissingPeople, id: \.id) { missingPerson in
+                        PeopleCardView(missingPerson: missingPerson)
+                            .listRowBackground(Color.clear)
+                            .listRowSeparator(.hidden)
+                    }
+                    .listStyle(.plain)
+                    .refreshable { updateMissingPeopleList() }
                 }
-                .listStyle(.plain)
-                .refreshable { updateMissingPeopleList() }
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("Missing People")
-            .toolbar {
-//                ToolbarItem(placement: .topBarTrailing) {
-//                    Button(action: {}) {
-//                        Text("SORTERA")
-//                            .font(.system(size: 10, weight: .semibold))
-//                            .padding(10)
-//                            .opacity(0.5)
-//                            .background {
-//                                RoundedRectangle(cornerRadius: 5)
-//                                    .fill(Color("SemiBackground"))
-//                            }
-//                    }
-//                }
+            .onReceive(vm.$text.debounce(for: .seconds(0.3), scheduler: DispatchQueue.main)) { newSearchText in
+                if newSearchText.isEmpty {
+                    filteredMissingPeople = missingPeople
+                } else {
+                    filteredMissingPeople = missingPeople.filter({
+                        let name = $0.name.lowercased()
+                        let searchKey = newSearchText.lowercased()
+                        if let lastSeenAt = $0.lastSeenAt?.lowercased() {
+                            return name.contains(searchKey) || lastSeenAt.contains(searchKey)
+                        } else {
+                            return name.contains(searchKey)
+                        }
+                    })
+                }
             }
             .onAppear {
                 if missingPeople.isEmpty { updateMissingPeopleList() }
